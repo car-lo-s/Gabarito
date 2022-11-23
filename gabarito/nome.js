43571a351f8ce973e9f9a7d0cfb75a1a795a81fa
Carlos Renato <64719682+car-lo-s@users.noreply.github.com>
2022-11-23 10:24:05 -0300

    Delete gabarito directory

--- gabarito/nome.js
+++
@@ -1,7 +0,0 @@
-let nomes = [
-    {id:1,nome:"Carlos",resposta:""},
-    {id:2,nome:"Larissa",resposta:""},
-    {id:3,nome:"Mateus",resposta:""},
-    {id:4,nome:"Tiago",resposta:""},
-    {id:5,nome:"João",resposta:""}
-]
